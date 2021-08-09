82f834be50e66eb578491bdd76752152b2ddd8c9
Ana <bia_espejo@hotmail.com>
2021-08-09 13:43:16 -0300

    projetos 09.08

---
+++ java/AULAS/src/Encapsulamento/Funcionario.java
@@ -0,0 +1,22 @@
+package Encapsulamento;
+
+public class Funcionario {
+	//atributos
+	private double salario;
+	private String nome;
+	
+	//get e set
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
