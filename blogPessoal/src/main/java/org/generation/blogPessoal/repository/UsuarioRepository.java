5cbc55a6db889f1b3a3901e645e88c3f4b5f2644
AnnaBia <bia_espejo@hotmail.com>
2021-09-03 13:59:37 -0300

    security

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package org.generation.blogPessoal.repository;
+
+import java.util.Optional;
+
+import org.generation.blogPessoal.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//INTERFACE REPOSITORY RESPONSÁVEL PELA COMUNICAÇÃO DA API COM O BD
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	// BUSCAR DADOS PELO USUARIO SEM LEVAR EM CONSIDERAÇÃO LETRA MAIÚSCULA OU MINÚSCULA
+	public Optional<Usuario> findByUsuario(String usuario);
+}
